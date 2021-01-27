cba2bf0dd861c0aef83fc04610a1a08419ebfac8
nazarja <mail.seanmurphy@gmail.com>
2021-01-27 18:11:43 +0000

    completed challenges up to no. 39

---
+++ Composition/src/Bedroom.java
@@ -0,0 +1,46 @@
+public class Bedroom {
+
+    private String name;
+    private Wall wall1;
+    private Wall wall2;
+    private Wall wall3;
+    private Wall wall4;
+    private Ceiling ceiling;
+    private Bed bed;
+    private Lamp lamp;
+
+    public Bedroom(String name, Wall wall1, Wall wall2, Wall wall3, Wall wall4, Ceiling ceiling, Bed bed, Lamp lamp) {
+        this.name = name;
+        this.wall1 = wall1;
+        this.wall2 = wall2;
+        this.wall3 = wall3;
+        this.wall4 = wall4;
+        this.ceiling = ceiling;
+        this.bed = bed;
+        this.lamp = lamp;
+    }
+
+    public Lamp getLamp() {
+        return lamp;
+    }
+
+    public void makeBed() {
+        System.out.println("Bedroom -> Making bed | ");
+        this.bed.make();
+    }
+
+    public static void main(String[] args) {
+        Wall wall1 = new Wall("West");
+        Wall wall2 = new Wall("East");
+        Wall wall3 = new Wall("South");
+        Wall wall4 = new Wall("North");
+        Ceiling ceiling = new Ceiling(12,  25);
+        Bed bed = new Bed("Modern", 4,3,2,1);
+        Lamp lamp = new Lamp("Classic", false, 75);
+
+        Bedroom bedroom = new Bedroom("Sean", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
+
+        bedroom.makeBed();
+        bedroom.getLamp().turnOn();
+    }
+}
